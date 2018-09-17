3f299d660a399b8d2f649c98498b991c0e19c4bf
Administrator <Administrator@USER-20180625OY>
2018-09-17 09:25:43 +0800

    ViewCoponent & Taghelper Library

---
+++ ClientComponent/WinterTasksTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Winter.Component
+{
+    public class WinterTasksTagHelper : TagHelper
+    {
+
+        public string Message { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+            output.Content.AppendHtml($"<h4>{Message}</h4>");
+        }
+    }
+}
+
